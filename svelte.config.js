6bd08f323dff1147b5a71c756e3afbc63e1d715c
Scott Benton <scott@scottbenton.dev>
2025-11-26 18:26:16 -0500

    fix(build): Fixed cloudflare build

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
